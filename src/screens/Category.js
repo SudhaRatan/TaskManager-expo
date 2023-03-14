12560c91b0b6197998e1ac171b6b0bb92a9e8bc2
SudhaRatan <rattankumar2511@gmail.com>
2023-03-14 19:43:20 +0530

    qwe

--- src/screens/Category.js
+++ src/screens/Category.js
@@ -4,21 +4,34 @@ import {
   Text,
   ScrollView,
   Dimensions,
+  Animated,
 } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import CircularProgressBase from 'react-native-circular-progress-indicator';
 import { TouchableOpacity } from 'react-native';
+import { useRef } from 'react';
 
 const Category = ({ route, navigation }) => {
 
-  const { height } = Dimensions.get('window')
+  const val = useRef(new Animated.Value(0)).current
+  const maxHeaderHeight = 160
+  const minHeaderHeight = 150
+
+  const data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  const { height, width } = Dimensions.get('window')
 
   return (
-    <ScrollView style={{ flex: 1, height: height * 2 }}>
-      <View style={{ height: StatusBar.currentHeight }} />
-      <View style={[
+    <View>
+      <StatusBar backgroundColor={route.params.color} />
+      <View style={{ height: StatusBar.currentHeight, backgroundColor: route.params.color }} />
+      <Animated.View style={[
         St.Header,
-        {borderColor: route.params.color + '80' },
+        {
+          height: minHeaderHeight,
+          backgroundColor: route.params.color
+        }
+        // { borderColor: route.params.color + '80' },
       ]}>
         <TouchableOpacity
           style={St.backCont}
@@ -26,24 +39,45 @@ const Category = ({ route, navigation }) => {
         >
           <Entypo name='chevron-left' size={40} />
         </TouchableOpacity>
-        <View style={St.CatCont}>
+        <View style={[
+          St.CatCont,
+        ]}>
           <View style={St.titleCont}>
             <Text style={St.title}>{route.params.name}</Text>
           </View>
           <View style={St.progressCont}>
             <CircularProgressBase
-              radius={20}
+              radius={16}
               value={route.params.progress * 100}
               inActiveStrokeOpacity={0.4}
               progressValueColor={'#00000000'}
-              activeStrokeColor={route.params.color}
-              // inActiveStrokeWidth={8}
-              duration={500}
+              activeStrokeColor={'#000'}
+              duration={600}
             />
           </View>
         </View>
-      </View>
-    </ScrollView>
+      </Animated.View>
+      <ScrollView
+        // scrollEventThrottle={16}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: val } } }],
+          { useNativeDriver: false },
+        )}
+      >
+        {
+          data.map((item) => {
+            return (
+              <View key={item} style={{
+                backgroundColor: '#456678',
+                width: width,
+                height: 100,
+                marginVertical: 5
+              }} />
+            )
+          })
+        }
+      </ScrollView>
+    </View>
   )
 }
 
@@ -51,39 +85,36 @@ export default Category;
 
 const St = StyleSheet.create({
   Header: {
-    flexDirection: 'row',
+    // flexDirection:'row',
     borderBottomWidth: 1,
-    paddingBottom: 10,
+    justifyContent: 'center',
+    // alignItems:'center'
   },
   title: {
     fontSize: 28,
-    height: null,
     fontWeight: 600,
-    flex: 1,
-    
-  },
-  titleCont: {
-    flex: 1,
-    justifyContent:"center",
-    alignItems:"flex-start",
   },
   backCont: {
     paddingLeft: 15,
     paddingRight: 5,
-    flex: 1,
-    alignItems:'flex-start',
-    justifyContent:'flex-start'
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start'
+  },
+  titleCont: {
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'flex-end',
   },
   progressCont: {
-    paddingRight: 15,
-    paddingLeft: 5,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-end'
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'flex-start',
   },
   CatCont: {
-    flex: 2,
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-evenly',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
   }
 })
