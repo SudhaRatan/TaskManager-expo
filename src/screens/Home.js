9a595487c813307fe83b671fbbc0dc05e49feb8f
SudhaRatan <rattankumar2511@gmail.com>
2023-03-20 01:35:56 +0530

    initial

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -17,8 +17,128 @@ import { useFocusEffect } from '@react-navigation/native';
 import { ActivityIndicator } from 'react-native';
 import { deleteCategory, tt, deleteTask } from '../db-functions/db';
 import { ToastAndroid } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import SQLite from 'react-native-sqlite-storage';
+
+// SQLite.enablePromise(true)
+
+const db = SQLite.openDatabase(
+  { name: 'Main1.db', location: 'default' },
+  () => { },
+  err => {
+    console.log(err)
+  }
+)
 
 const Home = ({ navigation }) => {
+
+  const CreateCatTable = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE if not exists categories (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          iconColor VARCHAR,
+          iconName VARCHAR,
+          name VARCHAR
+        );`,
+        [],
+        (tx, res) => {
+          // console.log(tx, res)
+        },
+        err => {
+          console.log(err)
+        }
+      )
+    })
+  }
+
+  const insertVals = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `insert into categories (name, iconColor, iconName) values ( "Work","#F08A5D","briefcase" );`, []
+      );
+      tx.executeSql(
+        `insert into categories (name, iconColor, iconName) values ( "Hostel","#3067C0","home" );`, [],
+        (res, ress) => {
+          console.log(res, ress)
+        },
+        err => console.log(err)
+      );
+    })
+  }
+
+  const getCats = () => {
+    setLoading(true)
+    setCategories(null)
+    db.transaction((tx) => {
+      tx.executeSql(
+        `select * from categories`, [],
+        (tx, res) => {
+          console.log(res.rows.item)
+          let len = res.rows.length
+          if (len > 0) {
+            let results = []
+            for (let i = 0; i < len; i++) {
+              // console.log(res.rows.item(i))
+              results.push(res.rows.item(i))
+            }
+            setCategories(results)
+            setLoading(false)
+          }
+        },
+        err => {
+          console.log(err)
+        }
+      )
+    })
+  }
+
+  const getCatsAsync = () => {
+    return new Promise((resolve, reject) => {
+      db.transaction((tx) => {
+        tx.executeSql(
+          `select * from categories`, [],
+          (tx, res) => {
+            let len = res.rows.length
+            if (len > 0) {
+              let results = []
+              for (let i = 0; i < len; i++) {
+                results.push(res.rows.item(i))
+              }
+              resolve(results)
+            }
+          },
+          err => {
+            console.log(err)
+            reject(err)
+          }
+        )
+      })
+    })
+  }
+
+  const gca = async () => {
+    setLoading(true)
+    setCategories(null)
+    const results = await getCatsAsync();
+    setCategories(results)
+    setLoading(false)
+  }
+
+  const isDrawerOpen = useDrawerStatus() === 'open';
+
+  useEffect(() => {
+    drawerAnim();
+    // CreateCatTable();
+    // insertVals();
+  }, [isDrawerOpen]);
+
+  useFocusEffect(useCallback(() => {
+    getCategoriesFunc();
+    getLatestTasks();
+    gca();
+    // getCats();
+  }, []))
 
   const [tasks, setTasks] = useState(null)
   const [loading, setLoading] = useState(true)
@@ -31,10 +151,17 @@ const Home = ({ navigation }) => {
   }
 
   const getCategoriesFunc = async () => {
-    const res = await getCategories()
-    if (res.stat) setCategories(res.res)
-    else setCategories(null)
-    setLoading(false)
+    // setCategories(null)
+    // setLoading(true)
+    // const res = await getCategories()
+    // if (res.stat) {
+    //   // const ress = await AsyncStorage.setItem('Categories', JSON.stringify(res.res))
+    //   setCategories(res.res)
+    // }
+    // else setCategories(null)
+    // const res = JSON.parse(await AsyncStorage.getItem('Categories'))
+    // setCategories(res)
+    // setLoading(false)
   }
 
   const enableTaskButton = () => {
@@ -43,24 +170,23 @@ const Home = ({ navigation }) => {
   }
 
   const getLatestTasks = async () => {
+    setLoadingTasks(true)
     setTasks(null)
-    tt.find({}).sort({ Date: -1 }).limit(10).exec((err, res) => {
-      if (res.length > 0) setTasks(res)
-      else setTasks(null)
-      setLoadingTasks(false)
-    })
+    // tt.find({}).sort({ Date: -1 }).limit(10).exec(async(err, res) => {
+    //   if (res.length > 0) {
+    //     setTasks(res)
+    //     // const ress = await AsyncStorage.setItem('Tasks', JSON.stringify(res))
+    //   }
+    //   else setTasks(null)
+    //   setLoadingTasks(false)
+    // })
+    const res = JSON.parse(await AsyncStorage.getItem('Tasks'))
+    // console.log(res)
+    setTasks(res)
+    setLoadingTasks(false)
+
   }
 
-  const isDrawerOpen = useDrawerStatus() === 'open';
-
-  useEffect(() => {
-    drawerAnim();
-  }, [isDrawerOpen]);
-
-  useFocusEffect(useCallback(() => {
-    getCategoriesFunc();
-    getLatestTasks();
-  }, []))
 
   const drawerAnim = () => {
     if (!isDrawerOpen) {
@@ -103,6 +229,8 @@ const Home = ({ navigation }) => {
     getLatestTasks();
   }
 
+
+
   return (
     <View style={{ flex: 1, backgroundColor: '#111E53', height: height, }} >
       <Anim.ScrollView
@@ -144,7 +272,7 @@ const Home = ({ navigation }) => {
                       {
                         categories.map((item, index) => {
                           return (
-                            <CategoryCard key={item._id} {...item} index={index} deleteCategory={deleteCategoryById} change={change} changeState={changeState} />
+                            <CategoryCard key={item.id} {...item} index={index} deleteCategory={deleteCategoryById} change={change} changeState={changeState} />
                           )
                         })
                       }
@@ -163,25 +291,26 @@ const Home = ({ navigation }) => {
             <Text style={St.taskText}>RECENTLY ADDED TASKS</Text>
             <View>
               {
-                tasks
+                loadingTasks
                   ?
-                  <View>
-                    {
-                      tasks.map((item, index) => {
-                        return (
-                          <TaskCard key={item._id} index={index} handleDelete={handleDelete} {...item} change={change} changeState={changeState} />
-                        )
-                      })
-                    }
-                    <View style={{ height: addButtonHeight }} />
+                  <View style={St.loadingSt}>
+                    <ActivityIndicator size="large" />
                   </View>
                   :
-                  loadingTasks
+                  tasks
                     ?
-                    <View style={St.loadingSt}>
-                      <ActivityIndicator size="large" />
+                    <View>
+                      {
+                        tasks.map((item, index) => {
+                          return (
+                            <TaskCard key={item._id} index={index} handleDelete={handleDelete} {...item} change={change} changeState={changeState} />
+                          )
+                        })
+                      }
+                      <View style={{ height: addButtonHeight }} />
                     </View>
                     :
+
                     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 50 }}>
                       <Text style={St.categoriesText}>Add Tasks to display here</Text>
                     </View>
